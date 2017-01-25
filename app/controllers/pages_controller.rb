e942cfc7a8bd4725304e2d554338fef1471a0998
Girlsday <girlsday@Jans-MacBook-Pro-2.local>
2017-01-25 09:10:51 +0100

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def main
+  end
+  def general
+  end
+  def general2
+  end
+  def destinations
+  end
+  def construction
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'pages#main'
+  get 'pages/general'
+  get 'pages/general2'
+  get 'pages/construction'
+  get 'pages/destinations'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
